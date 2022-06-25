2d646f378e0fa3f1547e9fd1e7860a08510b2757
Daler-01 <108170856+01-Tolibovich@users.noreply.github.com>
2022-06-25 22:10:20 +0500

    check

--- js/7.js
+++ js/7.js
@@ -9,3 +9,27 @@ while (i < scores.length) {
     console.log (output);
     i = i + 1;
 }
+
+function chekAge() {
+    let age = prompt ("Введите ваш возрост");
+    let name = prompt ("Введите ваше имя");
+    let sex = prompt ("Ваш пол. М или Ж ?");
+
+    if (age >= 18) {
+        if (sex == "М" || sex == "м" ) {
+            document.write ('<h1 class="title">Добро пожаловать ' + "господин " + name + "</h1>");
+            document.write (" Контент загружается, ждите...");
+        } else if (sex == "Ж" || sex == "ж" ) {
+            document.write ("<h1> Добро пожаловать " + "госпожа " + name + "</h1>");
+            document.write (" Контент загружается, ждите...");
+        }
+        
+    } else if ( name == null || name == "") {
+        document.write ("<h1> Отмена операции </h1>");
+    } else if (age < 18) {
+        document.write ("<h1> Не так быстро " + name + "</h1>");
+        document.write (" Контент для тебя не доступен!");
+    } 
+}
+
+chekAge ();
