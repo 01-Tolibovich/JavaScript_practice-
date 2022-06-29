f755ea2deb4ba84c3d3d5e400bc500f2a0eee443
01.tolibovich@gmail.com <01.tolibovich@gmail.com>
2022-06-29 10:18:31 +0600

    check

--- js/check.js
+++ js/check.js
@@ -1,17 +1,7 @@
-var scores = [60, 50, 60, 58, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 52, 55, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44]
 
-var output;
-
-var i = 0;
-
-while (i < scores.length) {
-    output = "Bubble solution #" + i + " score: " + scores[i];
-    console.log (output);
-    i = i + 1;
-}
 
 function chekAge() {
-    let age = prompt ("Введите ваш возрост");
+    let age = smoke.prompt ("Введите ваш возрост");
     let name = prompt ("Введите ваше имя");
     let sex = prompt ("Ваш пол. М или Ж ?");
 
